5db0e73f953330395a418b14a3b9b3765ce35a8c
pdurgaprasad <49471166+pdurgaprasad@users.noreply.github.com>
2019-04-10 14:22:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,12 @@
-/*
- */
-
-#include <avr/io.h>
-#include <util/delay.h>
-
-int main(void)
+#include<stdio.h>
+#include<string.h>
+main()
 {
-
-    // Insert code
-    DDRD&=~(1<<0);
-    PORTD|=(1<<0);
-    DDRD|=(1<<1);
-
-    while(1)
-    {
-        if(!(PIND &1))
-        {
-            PORTD|=(1<<1);
-            _delay_ms(2000);
-        }
-        else
-        {
-            PORTD&=~(1<<1);
-        _delay_ms(2000);
-    }
-
-    return 0;
-}
+    int a,b,c;
+    char str="prasad";
+    char arr="durga";
+    if(strcmp(str,arr)==0)
+        a=b=c=0;
+    else
+        a=b=c=1;
 }
